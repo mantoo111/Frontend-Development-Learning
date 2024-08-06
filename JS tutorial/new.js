546408cf4e0941519799db399480c108965eacf7
Ansa Abid <mantoo0201@gmail.com>
2024-08-06 15:50:49 +0500

    Introduction to Js

---
+++ JS tutorial/new.js
@@ -0,0 +1,4 @@
+console.log("hello world")
+console.log("code is running like...")
+console.log("code is running like a wao..")
+console.log("code is running")
---
+++ JS tutorial/script.js
@@ -0,0 +1,13 @@
+alert("Hello Word");
+console.log("code is running")
+var a=prompt("Enter your number")
+
+var isTrue =confirm("Are you sure you want to leave this page and blast your computer")
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("your number is " + a)
+//document.body.style.backgroundColor ="red"
