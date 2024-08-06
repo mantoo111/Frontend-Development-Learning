6914cf3553a3d295a322be558e0877c1cbc0ae06
Ansa Abid <mantoo0201@gmail.com>
2024-08-06 16:49:57 +0500

    variables, datatype and objects

---
+++ JS tutorial/variables,objects and data type/script.js
@@ -0,0 +1,31 @@
+console.log("hey this is intro to Js")
+// var  a= 5;
+let a=5;
+let b =6;
+console.log(a+b)
+var c ="Ansa"
+console.log(typeof a ,typeof b,typeof c)
+{
+    let a =66;
+    console.log(a)//only in the block
+}
+console.log(a)
+
+let x ="ansa"
+let y= 22;
+let z= 3.55;
+
+const p= true;
+let q= undefined;
+let r =null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x, typeof y , typeof z, typeof p, typeof q,typeof r)
+
+let o ={
+    name:"ansa",
+    "job role":509,
+    "is_handsome" : true
+}
+console.log(o)
+o.salary=20000
+console.log(o)
