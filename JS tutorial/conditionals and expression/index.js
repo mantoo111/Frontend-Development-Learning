efac650d5871766d64f8d0ebe8284b5724dc4657
Ansa Abid <mantoo0201@gmail.com>
2024-08-07 14:09:56 +0500

    conditionals and expression

---
+++ JS tutorial/conditionals and expression/index.js
@@ -0,0 +1,22 @@
+console.log("hey i am conditional tutorial")
+
+let age =45;
+let grace =2;
+age += grace
+console.log(age)
+console.log(age + grace)
+console.log(age - grace)
+console.log(age % grace)
+console.log(age / grace)
+console.log(age ** grace)//exponential
+
+if((age-grace)>18){
+     console.log("you can drive")
+}
+else{
+    console.log("you can not drive")
+}
+let a=9;
+let b=2;
+let c = a>b ?(a+b):(b-a);
+console.log("answr of c is", c )
