998da9beec1d08fbce351fd70031c9f01f8fe546
Ansa Abid <mantoo0201@gmail.com>
2024-08-07 16:24:24 +0500

    loops

---
+++ JS tutorial/Loops/index.js
@@ -0,0 +1,40 @@
+
+ let a =1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+
+// for (let index = 0; index < 100; index++) {
+//     console.log(a+index);
+    
+// }
+// let obj= {
+//     name:"ansa",
+//     role:"programmer",
+//     company:'iffionix'
+// }
+// //for in loop
+// for (const key in obj) {
+    
+//         const element = obj[key];
+//         console.log(key,element)
+        
+//     }
+// //for of loop is used in array for iteration purpose
+//     for (const c of "ansa") {
+//         console.log(c)
+        
+//     }
+// let i=0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+    
+// }
+let i=0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+
