e9670c30f45cf77d4086d5301784fe1f78c333b2
Ansa Abid <mantoo0201@gmail.com>
2024-08-09 15:53:36 +0500

    Strings

---
+++ JS tutorial/Strings/index.js
@@ -0,0 +1,26 @@
+console.log("this is string tutorial")
+
+let a="Ansa"
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+// console.log(a[5]) undefine
+console.log(a.length)
+let real_name="Ansa"
+let friend="Aaminah"
+console.log("his name is "+ real_name + " his friend name is "+ friend)
+console.log(`his name is ${real_name} and hisfriend name is ${friend}`)//it is known as template letrals ,used when to reduce the above line 
+let b= "asma"
+console.log(b.toUpperCase())
+console.log(b.length)
+console.log(b.slice(1,3))//slice
+console.log(b.slice(1))
+console.log(b.replace("as","34"))//replace the text with
+console.log(b.concat(a, "abid","latif"))
+let c= "  asma abid"
+console.log(c.trim())
+console.log(b.indexOf("ma"))
+console.log(b.startsWith("as"))
+console.log(c.endsWith("as"))
