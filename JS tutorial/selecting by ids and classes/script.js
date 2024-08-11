e314bf2e08af7ed474a7b37f365fa332f5e79a8d
Ansa Abid <mantoo0201@gmail.com>
2024-08-11 20:52:47 +0500

    DOM-id & classes

---
+++ JS tutorial/selecting by ids and classes/script.js
@@ -0,0 +1,13 @@
+// let a =  document.getElementsByClassName  ("box")
+// console.log(a)
+// a[2].style.backgroundColor="red"
+// document.getElementById("red").style.backgroundColor= "red"
+// document.querySelector(".box").style.backgroundColor="green"
+
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="green";
+    })
+
+    console.log(document.getElementsByTagName("div"))
+
+    document.querySelector(".container").contain(e[2])
