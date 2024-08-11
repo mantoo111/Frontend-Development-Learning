9e8d7c306a6a2940c910462a9b6881f27d211fce
Ansa Abid <mantoo0201@gmail.com>
2024-08-11 13:09:22 +0500

    DOM-parents,siblings and child nodes

---
+++ JS tutorial/DOM-parents ,siblings,children/index.js
@@ -0,0 +1,6 @@
+console.log("hello world")
+document.body.firstElementChild
+document.body.firstElementChild.childNodes//text+elemets+comments
+document.body.firstElementChild.children//only html 
+document.body.firstElementChild.children[3].nextElementSibling
+console.log(document.body.firstElementChild.children[3].previousSibling)
