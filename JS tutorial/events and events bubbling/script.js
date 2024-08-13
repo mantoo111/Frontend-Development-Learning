769ce2d67089d52dc22004cbd16090fbf0766c92
Ansa Abid <mantoo0201@gmail.com>
2024-08-13 18:36:41 +0500

    event and event bubbling,settime

---
+++ JS tutorial/events and events bubbling/script.js
@@ -0,0 +1,5 @@
+let b=document .getElementById("btn")
+b.addEventListener("click",()=>{
+alert("i was clicked")//or you can add function that is to be performed when button is clicked
+document.querySelector(".box").innerHTML="yay you are clicked"
+})
