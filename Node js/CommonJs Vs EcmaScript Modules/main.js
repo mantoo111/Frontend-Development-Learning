93122f95ac8e211db7493df2ae72b6b25ad8a23b
Ansa Abid <mantoo0201@gmail.com>
2024-08-27 13:52:03 +0500

    CommonJs Vs EcmaScript Modules

---
+++ Node js/CommonJs Vs EcmaScript Modules/main.js
@@ -0,0 +1,11 @@
+
+// import {a} from "./mymodule.js"
+// console.log(a)
+//default :
+// import obj from "./mymodule.js"
+// console.log(obj)
+
+
+//concept of require:
+const a = require("./mymodule2.js")
+console.log(a)
